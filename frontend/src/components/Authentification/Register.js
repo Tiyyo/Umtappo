00b610241e75f4f8c956071a70e48967ad55e731
Steeve Matou <steeve.matou@gmail.com>
2023-02-11 21:37:03 +0100

    start react hook form

--- frontend/src/components/Authentification/Register.js
+++ frontend/src/components/Authentification/Register.js
@@ -3,47 +3,50 @@ import { useEffect } from "react";
 import { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-
+import { useForm } from "react-hook-form";
 const Register = () => {
-  const navigate = useNavigate();
-  const [username, setUsername] = useState(null);
-  const [email, setEmail] = useState(null);
-  const [password, setPassword] = useState(null);
-  const [confirmedPassword, setConfirmedPassword] = useState(null);
-  const [error, setError] = useState(null);
-  const [errorUsername, setErrorUsername] = useState(null);
-  const [errorEmail, setErrorMail] = useState(null);
-  const [isValidate, setIsValidate] = useState(false);
+  // const navigate = useNavigate();
 
-  const sendUserInfos = async (e) => {
-    e.preventDefault();
-    await axios
-      .post("http://localhost:5000/user/register", {
-        username: username?.toString(),
-        email: email?.toString(),
-        password: password?.toString(),
-      })
-      .then((res) => {
-        if (res.status === 201) {
-          alert("You are now registerd !");
-        }
-      })
-      .catch((err) => {
-        console.log(err.response.data);
-        setError(err.response.data);
-        setIsValidate(false);
-      });
-    if (isValidate) {
-      navigate("/login");
-    } else {
-      console.log(error);
-    }
+  // const [username, setUsername] = useState(null);
+  // const [email, setEmail] = useState(null);
+  // const [password, setPassword] = useState(null);
+  // const [confirmedPassword, setConfirmedPassword] = useState(null);
+  // const [error, setError] = useState(null);
+  // const [errorUsername, setErrorUsername] = useState(null);
+  // const [errorEmail, setErrorMail] = useState(null);
+  // const [isValidate, setIsValidate] = useState(false);
+
+  const { register, handleSubmit, formState } = useForm();
+  const { isSubmitting } = formState;
+
+  const onSubmit = (data) => {
+    console.log(data);
   };
 
-  // useEffect(() => {
-  //   setErrorMail("This email is already used ");
-  //   setErrorUsername("Username already taken");
-  // });
+  // const sendUserInfos = async (e) => {
+  //   e.preventDefault();
+  //   await axios
+  //     .post("http://localhost:5000/user/register", {
+  //       username: username?.toString(),
+  //       email: email?.toString(),
+  //       password: password?.toString(),
+  //     })
+  //     .then((res) => {
+  //       if (res.status === 201) {
+  //         alert("You are now registerd !");
+  //       }
+  //     })
+  //     .catch((err) => {
+  //       console.log(err.response.data);
+  //       setError(err.response.data);
+  //       setIsValidate(false);
+  //     });
+  //   if (isValidate) {
+  //     navigate("/login");
+  //   } else {
+  //     console.log(error);
+  //   }
+  // };
 
   return (
     <div className="register-page">
@@ -52,61 +55,45 @@ const Register = () => {
           <h3>Sign Up for Free !</h3>
           <p>You'll get some special features designed just for you !</p>
         </div>
-        <form className="register-form">
+        <form className="register-form" onSubmit={() => handleSubmit(onSubmit)}>
           <div className="inputWrapper username">
             <input
               type="text"
+              name="username"
               className="inputUsername"
               placeholder="Username"
               required
-              onChange={(e) => setUsername(e.target.value)}
+              {...register("username")}
             />
-            <div className="error-container errorUsername">
-              {errorUsername ? errorUsername : ""}
-            </div>
+            <div className="error-container errorUsername"></div>
           </div>
-          {/* <div className="inputWrapper firstName">
-          <input type="text" className="firstName" placeholder="Jonh" required />
-          <div className="error-container errorFirstName"></div>
-        </div>
-        <div className="inputWrapper lastName">
-          <input type="text" className="lastName" placeholder="Doe" required />
-          <div className="error-container errorLastName"></div>
-        </div> */}
           <div className="inputWrapper email">
             <input
               type="email"
+              name="email"
               className="email"
               placeholder="john.doe@gmail.com"
               required
-              onChange={(e) => {
-                setEmail(e.target.value);
-              }}
+              {...register("email")}
             />
-            <div className="error-container errorEmail">
-              {errorEmail ? errorEmail : ""}
-            </div>
+            <div className="error-container errorEmail"></div>
           </div>
           <div className="inputWrapper password">
             <input
               type="password"
+              name="password"
               className="password"
               required
-              onChange={(e) => setPassword(e.target.value)}
+              {...register("password")}
             />
             <div className="error-container errorPassword"></div>
           </div>
           <div className="inputWrapper confirmed-password">
-            <input
-              type="password"
-              className="confirmed-password"
-              required
-              onChange={(e) => setConfirmedPassword(e.target.value)}
-            />
+            <input type="password" className="confirmed-password" required />
             <div className="error-container errorPassword"></div>
           </div>
 
-          <button type="submit" onClick={(e) => sendUserInfos(e)}>
+          <button type="submit" disabled={isSubmitting}>
             Valid your Inscription
           </button>
         </form>
