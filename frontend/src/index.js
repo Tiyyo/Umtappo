8bc485ad1f9220e8ae040546b05ec654bf6047fe
Tiyyo <111092370+Tiyyo@users.noreply.github.com>
2023-02-26 18:27:22 +0100

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,14 +4,24 @@ import "./index.css";
 import App from "./App";
 import { AppContextProvider } from "./utils/Context/AppContextProvider";
 import { UserContextProvider } from "./utils/Context/UserContextProvider";
+import { configureStore } from '@reduxjs/toolkit'
+import {provider} from "react-redux"
+
+const store = configureStore({
+  reducer : {
+    user : userListReducers,
+  }
+})
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <React.StrictMode>
+  <Provider>
   <AppContextProvider>
     <UserContextProvider>
       <App />
     </UserContextProvider>
   </AppContextProvider>
+  </Provider>
   // </React.StrictMode>
 );
