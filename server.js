7c3ab86e3eb3f4be745c7af4b99602a3a7d4a529
Steeve Matou <steeve.matou@gmail.com>
2023-04-13 23:28:22 +0200

    try fixed deploy 8

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.get("/s3Url", async (req, res) => {
 });
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.status("client/build"));
+  // app.use(express.status("client/build"));
   app.get("*", (req, res) =>
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
   );
