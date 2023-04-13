087dd39dc9b7f6b1480df3b5086283454fdaa37e
Steeve Matou <steeve.matou@gmail.com>
2023-04-14 00:39:17 +0200

    try fixed deploy 10

--- server.js
+++ server.js
@@ -3,27 +3,17 @@ const connectDB = require("./config/db");
 const dotenv = require("dotenv").config();
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
+const path = require("path");
 const port = process.env.PORT || 5000;
 const s3 = require("./controllers/s3");
 const allowedOrigins = require("./config/allowedOrigins");
+const corsOptions = require("./config/corsOptions");
 
 connectDB();
 
 const app = express();
 
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-        callback(null, true);
-      } else {
-        callback(new Erro("Not allowed by CORS"));
-      }
-    },
-    credentials: true,
-    optionsSuccesStatus: 200,
-  })
-);
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(cookieParser());
@@ -33,16 +23,20 @@ app.use("/list", require("./routes/list.routes"));
 app.use("/like", require("./routes/likes.routes"));
 app.use("/rate", require("./routes/rates.routes"));
 app.use("/recommendations", require("./routes/recommendations.routes"));
-// app.use(express.static(path.join(__dirname, "client", "build")));
+app.use(
+  express.static(path.join(__dirname, "/frontend", "public", "index.html"))
+);
 app.get("/s3Url", async (req, res) => {
   const url = await s3.generateUploadURL();
   res.send({ url });
 });
 
 if (process.env.NODE_ENV === "production") {
-  // app.use(express.status("client/build"));
+  app.use(
+    express.static(path.join(__dirname, "/frontend", "build", "index.html"))
+  );
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+    res.sendFile(path.resolve(__dirname, "./frontend", "build", "index.html"))
   );
 }
 app.get("/*", (req, res) => {
